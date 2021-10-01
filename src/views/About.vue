abd7c4efd6948621f020d50ad6625f78403008f0
trynio <trynio132@gmail.com>
2021-10-01 22:02:12 +0200

    A few of styles

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,6 +6,9 @@
   <main>
     <span>Title - About</span>
     <ListComponent></ListComponent>
+    <button class="float-button">
+      <VIcon name="plus" class="icon" scale="1.2" />
+    </button>
   </main>
   <footer>
     <Menu />
@@ -15,6 +18,8 @@
 
 <script>
 import ListComponent from '@/components/ListComponent.vue';
+import 'vue-awesome/icons';
+import VIcon from 'vue-awesome/components/Icon';
 import Menu from '../components/Menu.vue';
 import { auth } from '../firebase';
 
@@ -24,6 +29,7 @@ export default {
   components: {
     ListComponent,
     Menu,
+    VIcon,
   },
 
   computed: {
@@ -48,5 +54,46 @@ export default {
     display: flex;
     justify-content: flex-end;
     padding: 15px;
+  }
+  .float-button {
+    background: $primary;
+    color: $light;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    border: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9;
+    position: absolute;
+    bottom: 50px;
+    right: 20px;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+  }
+  .float-button:hover {
+    background: darken($primary, 5%);
+  }
+  .float-button:after {
+    content: "";
+    background: darken($primary, 35%);
+    display: block;
+    width: 50px;
+    height: 50px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    border-radius: 50%;
+    opacity: 0%;
+    transition: all 0.4s;
+  }
+  .float-button:active:after {
+    opacity: 50%;
+    width: 0px;
+    height: 0px;
+    left: 50%;
+    top: 50%;
+    transition: 0s
   }
 </style>
